b5154667e06d9df6752f914eef2eaee6e826240d
hung <bahung1tt@gmail.com>
2021-12-23 15:06:56 +1100

    chessboard is now playable

--- play.py
+++ play.py
@@ -2,22 +2,40 @@
 
 from flask import Flask, render_template, request
 import json
+import chess
+import random
 
 app = Flask(__name__, template_folder=".")
 
 @app.route("/")
 def hello_world():
   ret = open("index.html").read()
-  return ret
+  return ret.replace("start", board.fen())
 
-@app.route("/move_coordinate", methods=["POST"])
-def get_post_js_data():
+@app.route("/move_coordinate", methods=["GET"])
+def get_js_data():
+  source = request.args.get('source', default='')
   piece = request.args.get('piece', default='')
-  position = request.args.get("position")
-  source = request.args.get("source")
-  print(piece, position, source)
-  return app.response_class()
+  target = request.args.get('target', default='')
+
+  if not board.is_game_over():
+    # Human move
+    move = chess.Move.from_uci(source+target)
+    if move in board.legal_moves:
+      board.push(move)
+      if not board.is_game_over():
+        # Computer move
+        move = random.choice(list(board.legal_moves))
+        board.push(move)
+        print("\n ---Board--- \n{}\n\nStatus: {}\n".format(board, board.outcome()))
+        response = app.response_class(response=board.fen(), status=200)
+        return response
+
+  print("GAME OVER")
+  response = app.response_class(response="game over", status=200)
+  return response
 
 if __name__ == "__main__":
+  board = chess.Board()
   app.run(debug=True)
   
