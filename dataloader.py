b0c73737ac98c6737ea64d068dc04e719cc7fb9b
hbhungg <hung.ba.huynh@proton.me>
2024-10-15 18:03:17 +1100

    randomize the output order

--- dataloader.py
+++ dataloader.py
@@ -7,7 +7,8 @@ import random
 
 def get_bitboard(fen):
   board = chess.Board(fen)
-  SIZE = 64*6*2+5
+  # 8x8 board, each square has 6 state (for each piece), mul by 2 for color, and last 5 for misc data about game state
+  SIZE = 8*8*6*2+5
   bitboard = np.zeros(SIZE)
   piece_idx = {'p': 0, 'n': 1, 'b': 2, 'r': 3, 'q': 4, 'k': 5}
 
@@ -37,9 +38,10 @@ class BoardPairDataset:
   def __len__(self) -> int: return len(self.w)*len(self.b)
   def __getitem__(self, idx) -> tuple[Tensor, Tensor]:
     l = (idx.stop - idx.start) if isinstance(idx, slice) else 1
-    w = Tensor.cat(*[get_bitboard(x) for x in random.sample(self.w, l)], dim=0)
-    b = Tensor.cat(*[get_bitboard(x) for x in random.sample(self.b, l)], dim=0)
-    return w, b
+    rc = [(x, y, 1, 0) if random.choice([True, False]) else (y, x, 0, 1) for x, y in zip(random.sample(self.w, l),random.sample(self.b, l))]
+    b = Tensor.cat(*[Tensor.cat(get_bitboard(x[0]), get_bitboard(x[1])).expand(1, -1, -1) for x in rc])
+    r = Tensor.cat(*[Tensor(x[2:]).expand(1, -1) for x in rc])
+    return b, r
 
 class BoardDataset:
   def __init__(self, db:str, train=True, split=0.8):
@@ -47,15 +49,21 @@ class BoardDataset:
       with contextlib.closing(conn.cursor()) as cur:
         tlen = cur.execute("SELECT count(*) FROM boards;").fetchone()[0]
         s, e = (0, int(tlen*split)) if train else (int(tlen*split), tlen)
-        self.data = cur.execute(f"SELECT fen, result FROM boards WHERE id BETWEEN {s} AND {e};").fetchall()
+        self.data = [x[0] for x in cur.execute(f"SELECT fen FROM boards WHERE id BETWEEN {s} AND {e};").fetchall()]
 
   def __len__(self) -> int: return len(self.data)
   def __getitem__(self, idx) -> tuple[Tensor, Tensor]:
-    boards = Tensor.cat(*[get_bitboard(x) for x, _ in self.data[idx]], dim=0)
-    results = Tensor([float(y) for _, y in self.data[idx]])
-    return boards, results
+    return Tensor.cat(*[get_bitboard(x) for x in self.data[idx]], dim=0)
 
 class Dataloader:
   def __init__(self, ds, batch_size): self.ds, self.batch_size = ds, batch_size
   def __iter__(self): return (self.ds[x:x+self.batch_size] for x in range(0, len(self)*self.batch_size, self.batch_size))
   def __len__(self): return len(self.ds)//self.batch_size
+
+if __name__ == "__main__":
+  a = BoardPairDataset("dataset/dataset_100000.db")
+  dl = Dataloader(a, 512)
+  for x in dl:
+    print(x)
+    print(x[1].numpy())
+    break
